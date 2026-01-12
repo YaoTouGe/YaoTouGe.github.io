169d6e2a3c5d36bcf46fbedd9b65cdc27e262a8d
YaoTouGe <YaoTouGe@users.noreply.github.com>
2026-01-12 00:52:15 +0000

    deploy: 96eb01831f6dd01e78415d8d339e21ccb26cca0e

--- sw.js
+++ sw.js
@@ -24,8 +24,8 @@ const pages = [
   "/zh/docs/ltclight/light_types/textured_light/",
   "/zh/docs/ltclight/light_types/linear_light/",
   "/book.min.cc2c524ed250aac81b23d1f4af87344917b325208841feca0968fe450f570575.css",
-  "/zh.search-data.min.ff21ece292b9d9bf3aeac70c79a15b7947f17247378e1c4b822b7b5dc267eca3.json",
-  "/zh.search.min.5ecc1f58012580b347d166211556c3ca3d71c3ca8fe2ad54c9a03c1cddfdfafe.js",
+  "/zh.search-data.min.1e2e2d92a6f66906158694f96703cfdbbeaf4a83cf348ff6197d4e98e1ef062e.json",
+  "/zh.search.min.cfbb2722405e8d2c66dbb59157cf5d5307b7c86bb178c9595592f6629e80d207.js",
   
 ];
 
