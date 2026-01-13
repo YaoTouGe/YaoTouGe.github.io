fc1064ada78d0efdcfdc91ef76b06bbb21f64488
YaoTouGe <YaoTouGe@users.noreply.github.com>
2026-01-13 03:50:26 +0000

    deploy: 070ac64eb48865283381770eb70fad808cd48677

--- sw.js
+++ sw.js
@@ -9,6 +9,8 @@ const pages = [
   "/zh/docs/ltclight/integration/",
   "/docs/ltclight/light_types/",
   "/zh/docs/ltclight/light_types/",
+  "/docs/ltclight/global_config/",
+  "/zh/docs/ltclight/global_config/",
   "/zh/",
   "/",
   "/zh/categories/",
@@ -24,8 +26,8 @@ const pages = [
   "/zh/docs/ltclight/light_types/textured_light/",
   "/zh/docs/ltclight/light_types/linear_light/",
   "/book.min.cc2c524ed250aac81b23d1f4af87344917b325208841feca0968fe450f570575.css",
-  "/zh.search-data.min.1e2e2d92a6f66906158694f96703cfdbbeaf4a83cf348ff6197d4e98e1ef062e.json",
-  "/zh.search.min.cfbb2722405e8d2c66dbb59157cf5d5307b7c86bb178c9595592f6629e80d207.js",
+  "/zh.search-data.min.ad4fc37ed7af9e477faf9f2b8a1a2ad897934f1415d06eeb4d695e55a95a3eed.json",
+  "/zh.search.min.54e269c3ce26f67ee09ec3d27d1a1ba0e6b04be6e262b63a1a294aec555fd22a.js",
   
 ];
 
